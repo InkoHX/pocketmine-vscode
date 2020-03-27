62e6cdec0a40d6e7bd4eaf4ad5d77f7bbccc4e0c
InkoHX <inkohx@gmail.com>
2020-03-27 17:30:40 +0900

    fix: require autoload.

--- src/author/pluginName/Main.php
+++ src/author/pluginName/Main.php
@@ -6,6 +6,11 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase
 {
+  public function onLoad()
+  {
+    require $this->getFile() . "vendor/autoload.php";
+  }
+
   public function onEnable(): void
   {
     $this->logger->info('Hey');
